96dc467171d3167e6b5b90862df0d25a983849bb
sifnoc <sifnoc09@gmail.com>
2024-03-17 22:28:12 +0000

    refactor: more efficient benchmark flow

--- zk_prover/benches/full_solvency_flow.rs
+++ zk_prover/benches/full_solvency_flow.rs
@@ -1,161 +1,172 @@
 #![feature(generic_const_exprs)]
 use criterion::{criterion_group, criterion_main, Criterion};
 use halo2_proofs::plonk::{keygen_pk, keygen_vk};
+use rand::rngs::OsRng;
+use rand::Rng;
 use summa_solvency::{
     circuits::merkle_sum_tree::MstInclusionCircuit,
     circuits::{
         utils::{full_prover, full_verifier, generate_setup_artifacts},
         WithInstances,
     },
-    merkle_sum_tree::{MerkleSumTree, Tree},
+    merkle_sum_tree::{utils::generate_dummy_entries, Cryptocurrency, MerkleSumTree, Tree},
 };
 
-const SAMPLE_SIZE: usize = 10;
-const LEVELS: usize = 20;
-const N_CURRENCIES: usize = 1;
-const N_BYTES: usize = 8;
+fn bench_mst<
+    const K: u32,
+    const LEVELS: usize,
+    const N_USERS: usize,
+    const N_BYTES: usize,
+    const N_CURRENCIES: usize,
+>(
+    name: &str,
+) where
+    [(); N_CURRENCIES + 1]: Sized,
+    [(); N_CURRENCIES + 2]: Sized,
+{
+    let mut c = Criterion::default().sample_size(10);
 
-fn build_mstree(_c: &mut Criterion) {
-    let mut criterion = Criterion::default().sample_size(SAMPLE_SIZE);
+    let entries = generate_dummy_entries::<N_USERS, N_CURRENCIES>().unwrap();
 
-    let csv_file = format!("benches/csv/{}_entry_2_{}.csv", N_CURRENCIES, LEVELS);
+    let cryptocurrencies: [Cryptocurrency; N_CURRENCIES] =
+        std::array::from_fn(|_| Cryptocurrency {
+            name: "ETH".to_string(),
+            chain: "ETH".to_string(),
+        });
 
-    let bench_name = format!(
-        "build Merkle sum tree for 2 power of {} entries with {} currencies",
-        LEVELS, N_CURRENCIES
-    );
-
-    criterion.bench_function(&bench_name, |b| {
-        b.iter(|| {
-            MerkleSumTree::<N_CURRENCIES, N_BYTES>::from_csv(&csv_file).unwrap();
-        })
+    c.bench_function(&format!("{name} - build merkle sum tree"), |b| {
+        b.iter_batched(
+            || (entries.clone(), cryptocurrencies.to_vec()),
+            |(entries, cryptocurrencies)| {
+                let _ = MerkleSumTree::<N_CURRENCIES, N_BYTES>::from_entries(
+                    entries,
+                    cryptocurrencies,
+                    false,
+                )
+                .unwrap();
+            },
+            criterion::BatchSize::SmallInput,
+        );
     });
-}
 
-fn build_sorted_mstree(_c: &mut Criterion) {
-    let mut criterion = Criterion::default().sample_size(SAMPLE_SIZE);
-
-    let csv_file = format!("benches/csv/{}_entry_2_{}.csv", N_CURRENCIES, LEVELS);
-
-    let bench_name = format!(
-        "build sorted Merkle sum tree for 2 power of {} entries with {} currencies",
-        LEVELS, N_CURRENCIES
-    );
-
-    criterion.bench_function(&bench_name, |b| {
-        b.iter(|| {
-            MerkleSumTree::<N_CURRENCIES, N_BYTES>::from_csv_sorted(&csv_file).unwrap();
-        })
+    c.bench_function(&format!("{name} - build sorted merkle sum tree"), |b| {
+        b.iter_batched(
+            || (entries.clone(), cryptocurrencies.to_vec()),
+            |(entries, cryptocurrencies)| {
+                let _ = MerkleSumTree::<N_CURRENCIES, N_BYTES>::from_entries(
+                    entries,
+                    cryptocurrencies,
+                    true,
+                )
+                .unwrap();
+            },
+            criterion::BatchSize::SmallInput,
+        );
     });
-}
 
-fn verification_key_gen_mst_inclusion_circuit(_c: &mut Criterion) {
-    let mut criterion = Criterion::default().sample_size(SAMPLE_SIZE);
+    // Generate a random user index
+    let get_random_user_index = || {
+        let user_range: std::ops::Range<usize> = 0..N_USERS;
+        OsRng.gen_range(user_range)
+    };
 
-    let empty_circuit = MstInclusionCircuit::<LEVELS, N_CURRENCIES, N_BYTES>::init_empty();
+    let merkle_sum_tree = MerkleSumTree::<N_CURRENCIES, N_BYTES>::from_entries(
+        entries,
+        cryptocurrencies.to_vec(),
+        false,
+    )
+    .unwrap();
 
-    let (params, _, _) = generate_setup_artifacts(13, None, empty_circuit.clone()).unwrap();
-
-    let bench_name = format!(
-        "gen verification key for 2 power of {} entries with {} currencies mst inclusion circuit",
-        LEVELS, N_CURRENCIES
-    );
-    criterion.bench_function(&bench_name, |b| {
-        b.iter(|| {
-            keygen_vk(&params, &empty_circuit).expect("vk generation should not fail");
-        })
+    c.bench_function(&format!("{name} - generate merkle proof"), |b| {
+        b.iter_batched(
+            || (get_random_user_index()),
+            |user_index| {
+                let _ = merkle_sum_tree.generate_proof(user_index).unwrap();
+            },
+            criterion::BatchSize::SmallInput,
+        );
     });
-}
 
-fn proving_key_gen_mst_inclusion_circuit(_c: &mut Criterion) {
-    let mut criterion = Criterion::default().sample_size(SAMPLE_SIZE);
-
-    let empty_circuit = MstInclusionCircuit::<LEVELS, N_CURRENCIES, N_BYTES>::init_empty();
-
-    let (params, _, vk) = generate_setup_artifacts(13, None, empty_circuit.clone()).unwrap();
-
-    let bench_name = format!(
-        "gen proving key for 2 power of {} entries with {} currencies mst inclusion circuit",
-        LEVELS, N_CURRENCIES
-    );
-    criterion.bench_function(&bench_name, |b| {
-        b.iter(|| {
-            keygen_pk(&params, vk.clone(), &empty_circuit).expect("pk generation should not fail");
-        })
-    });
-}
-
-fn generate_zk_proof_mst_inclusion_circuit(_c: &mut Criterion) {
-    let mut criterion = Criterion::default().sample_size(SAMPLE_SIZE);
-
-    let empty_circuit = MstInclusionCircuit::<LEVELS, N_CURRENCIES, N_BYTES>::init_empty();
-
-    let (params, pk, _) = generate_setup_artifacts(13, None, empty_circuit).unwrap();
-
-    let csv_file = format!("benches/csv/{}_entry_2_{}.csv", N_CURRENCIES, LEVELS);
-
-    let merkle_sum_tree = MerkleSumTree::<N_CURRENCIES, N_BYTES>::from_csv(&csv_file).unwrap();
-
-    // Only now we can instantiate the circuit with the actual inputs
-
-    let user_index = 0;
+    let user_index = get_random_user_index();
 
     let merkle_proof = merkle_sum_tree.generate_proof(user_index).unwrap();
 
     let circuit = MstInclusionCircuit::<LEVELS, N_CURRENCIES, N_BYTES>::init(merkle_proof);
 
-    let bench_name = format!(
-        "generate zk proof - tree of 2 power of {} entries with {} currencies mst inclusion circuit",
-        LEVELS, N_CURRENCIES
-    );
-    criterion.bench_function(&bench_name, |b| {
-        b.iter(|| {
-            full_prover(&params, &pk, circuit.clone(), circuit.instances());
-        })
-    });
-}
-
-fn verify_zk_proof_mst_inclusion_circuit(_c: &mut Criterion) {
-    let mut criterion = Criterion::default().sample_size(SAMPLE_SIZE);
-
     let empty_circuit = MstInclusionCircuit::<LEVELS, N_CURRENCIES, N_BYTES>::init_empty();
 
-    let (params, pk, vk) = generate_setup_artifacts(13, None, empty_circuit).unwrap();
+    let (params, pk, vk) = generate_setup_artifacts(K, None, empty_circuit.clone()).unwrap();
 
-    let csv_file = format!("benches/csv/{}_entry_2_{}.csv", N_CURRENCIES, LEVELS);
+    c.bench_function(&format!("{name} - gen verification key"), |b| {
+        b.iter_batched(
+            || empty_circuit.clone(),
+            |empty_circuit| {
+                keygen_vk(&params, &empty_circuit).expect("vk generation should not fail");
+            },
+            criterion::BatchSize::SmallInput,
+        )
+    });
 
-    let merkle_sum_tree = MerkleSumTree::<N_CURRENCIES, N_BYTES>::from_csv(&csv_file).unwrap();
+    c.bench_function(&format!("{name} - gen proving key"), |b| {
+        b.iter_batched(
+            || (vk.clone(), empty_circuit.clone()),
+            |(vk, empty_circuit)| {
+                keygen_pk(&params, vk, &empty_circuit).expect("pk generation should not fail");
+            },
+            criterion::BatchSize::SmallInput,
+        )
+    });
 
-    // Only now we can instantiate the circuit with the actual inputs
+    c.bench_function(&format!("{name} - generate inclusion zk proof"), |b| {
+        b.iter_batched(
+            || (pk.clone(), vk.clone(), circuit.clone(), circuit.instances()),
+            |(pk, vk, circuit, instances)| {
+                full_prover(&params, &pk, circuit, instances);
+            },
+            criterion::BatchSize::SmallInput,
+        );
+    });
 
-    let user_index = 0;
-
-    let merkle_proof = merkle_sum_tree.generate_proof(user_index).unwrap();
-
-    let circuit = MstInclusionCircuit::<LEVELS, N_CURRENCIES, N_BYTES>::init(merkle_proof);
-
+    // verify inclusion zk proof
     let proof = full_prover(&params, &pk, circuit.clone(), circuit.instances());
 
-    println!("proof size in bytes: {}", proof.len());
-
-    let bench_name = format!(
-        "verify zk proof - tree of 2 power of {} entries with {} currencies mst inclusion circuit",
-        LEVELS, N_CURRENCIES
-    );
-    criterion.bench_function(&bench_name, |b| {
-        b.iter(|| {
-            full_verifier(&params, &vk, proof.clone(), circuit.instances());
-        })
+    c.bench_function(&format!("{name} - verify inclusion zk proof"), |b| {
+        b.iter_batched(
+            || {
+                (
+                    vk.clone(),
+                    proof.clone(),
+                    circuit.clone(),
+                    circuit.instances(),
+                )
+            },
+            |(vk, proof, circuit, instances)| {
+                full_verifier(&params, &vk, proof, instances);
+            },
+            criterion::BatchSize::SmallInput,
+        );
     });
 }
 
-criterion_group!(
-    benches,
-    build_mstree,
-    build_sorted_mstree,
-    verification_key_gen_mst_inclusion_circuit,
-    proving_key_gen_mst_inclusion_circuit,
-    generate_zk_proof_mst_inclusion_circuit,
-    verify_zk_proof_mst_inclusion_circuit,
-);
+fn criterion_benchmark(_c: &mut Criterion) {
+    const K: u32 = 13;
+    {
+        const N_CURRENCIES: usize = 1;
+        const LEVELS: usize = 18;
+        const N_USERS: usize = 2usize.pow(LEVELS as u32) - 6;
+        bench_mst::<K, LEVELS, N_USERS, 8, N_CURRENCIES>(
+            format!("K = {K}, N_USERS = {N_USERS}, N_CURRENCIES = {N_CURRENCIES}").as_str(),
+        );
+    }
+    {
+        const N_CURRENCIES: usize = 350;
+        const LEVELS: usize = 18;
+        const N_USERS: usize = 2usize.pow(LEVELS as u32) - 6;
+        bench_mst::<K, LEVELS, N_USERS, 8, N_CURRENCIES>(
+            format!("K = {K}, N_USERS = {N_USERS}, N_CURRENCIES = {N_CURRENCIES}").as_str(),
+        );
+    }
+}
+
+criterion_group!(benches, criterion_benchmark,);
 criterion_main!(benches);
--- zk_prover/src/merkle_sum_tree/utils/mod.rs
+++ zk_prover/src/merkle_sum_tree/utils/mod.rs
@@ -1,7 +1,9 @@
 mod build_tree;
 mod csv_parser;
+mod dummy_entries;
 mod operation_helpers;
 
 pub use build_tree::{build_leaves_from_entries, build_merkle_tree_from_leaves};
 pub use csv_parser::parse_csv_to_entries;
+pub use dummy_entries::generate_dummy_entries;
 pub use operation_helpers::*;
